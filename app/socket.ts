3f69b85df662f90a5590d2f61abc17cb6ee26f04
Mohamed Khaled <oagitalsolutions@gmail.com>
2024-06-13 22:22:05 +0400

    fix lint issue

--- app/socket.ts
+++ app/socket.ts
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.SERVER_URL as string;
+const URL = process.env.NEXT_PUBLIC_SERVER_URL as string;
 console.log("CONNECTED TO : ", URL);
 export const socket = io(URL, {
   transports: ["websocket"],
